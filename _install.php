cf50292f44aa560ca71e4db35f6ad046f5dea665
franck <carnet.franck.paul@gmail.com>
2022-12-26 18:42:32 +0100

    New install

--- _install.php
+++ _install.php
@@ -15,26 +15,17 @@ if (!defined('DC_CONTEXT_ADMIN')) {
     return;
 }
 
-$m_version = dcCore::app()->plugins->moduleInfo('retrocontrol', 'version');
-$i_version = dcCore::app()->getVersion('retrocontrol');
-
-if (version_compare((string) $i_version, $m_version, '>=')) {
+if (!dcCore::app()->newVersion(basename(__DIR__), dcCore::app()->plugins->moduleInfo(basename(__DIR__), 'version'))) {
     return;
 }
 
 # --INSTALL AND UPDATE PROCEDURES--
 try {
-    dcCore::app()->blog->settings->addNamespace('retrocontrol');
-
     # New install / update (just erase settings - but not their values)
     dcCore::app()->blog->settings->retrocontrol->put('rc_sourceCheck', false, 'boolean', 'Check trackback source', false, true);
     dcCore::app()->blog->settings->retrocontrol->put('rc_timeoutCheck', false, 'boolean', 'Use disposable URL for trackbacks', false, true);
     dcCore::app()->blog->settings->retrocontrol->put('rc_recursive', true, 'boolean', 'Recursive filtering while checking source', false, true);
     dcCore::app()->blog->settings->retrocontrol->put('rc_timeout', 300, 'integer', 'Trackback URL time life (in seconds)', false, true);
-
-    # --SETTING NEW VERSION--
-
-    dcCore::app()->setVersion('retrocontrol', $m_version);
 
     return true;
 } catch (Exception $e) {
