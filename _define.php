c970850caa953a3e29e9d0752592c9aa2e670de2
franck <carnet.franck.paul@gmail.com>
2023-07-15 16:59:37 +0200

    New release 4.3

--- _define.php
+++ _define.php
@@ -19,7 +19,7 @@ $this->registerModule(
     'Rétrocontrôle',
     'Trackback validity check',
     'Alain Vagner, Oleksandr Syenchuk',
-    '4.2.1',
+    '4.3',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => dcCore::app()->auth->makePermissions([
